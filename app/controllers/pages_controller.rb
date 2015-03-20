b163efb56e790301640544ccd69cdaaeda5e2bcc
Kathryn Rees <kathryn.e.rees@gmail.com>
2015-03-20 14:54:00 -0500

    About

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,8 @@ class PagesController < ApplicationController
 
 	def about
 	end
+
+	def resume
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/contact' => 'pages#contact'
   get '/blog' => 'pages#blog'
   get '/about' => 'pages#about'
+  get '/resume' => 'pages#resume'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
